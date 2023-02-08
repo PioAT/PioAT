9ded79ac5e63363241c318556494800a60d8bff2
Caleb Ng 244611 <244611@aaps.internal>
2023-02-08 10:33:49 -0500

    multipage basis

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import { join, parse, resolve } from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -20,6 +22,26 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
       vue: "@vue/compat",
+      "~": __dirname,
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: entryPoints(
+        "index.html",
+        "2023Projects/index.html",
+      ),
     },
   },
 });
+
+function entryPoints(...paths) {
+  const entries = paths.map(parse).map(entry => {
+    const { dir, base, name, ext } = entry;
+    const key = join(dir, name);
+    const path = resolve(__dirname, dir, base);
+    return [key, path];
+  });
+  const config = Object.fromEntries(entries);
+  return config;
+}
