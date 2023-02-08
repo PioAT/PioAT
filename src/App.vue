be129f78ffb67f7da3f15ba88efbde0454c0ca3c
Justin Hu 321191 <321191@aaps.internal>
2023-02-08 10:59:48 -0500

    Add navbar template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
-<script setup>
 
+<script setup>
+import Navbar from "~/src/components/Navbar.vue";
 
 
 
@@ -7,113 +8,15 @@
 
 
 <template>
-  <header>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand class = "AT" href="#">Advanced Topics</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#">Projects</b-nav-item>
-          <b-nav-item href="#">Course overview</b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-
-     
-        <b-navbar-nav class="ml-auto">
-          <!-- Search button and input
-
-          <b-nav-form>
-            <b-form-input
-              size="sm"
-              class="mr-sm-2"
-              placeholder="Search"
-            ></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit"
-              >Search</b-button
-            >
-          </b-nav-form>
-        -->
-   
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em>User</em>
-            </template>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </header>
-
-  <main>
-    Advanced Topics For Computer Science
-    <div class="wrapper"> Welcome to advanced topics in computer science, a </div>
-  </main>
-
-
-
-
-
-
-  <div>
-    <!-- Website INSIDE the website
-  <b-embed
-    type="iframe"
-    aspect="16by9"
-    src= https://www.google.com/
-    allowfullscreen
-  ></b-embed>
-    -->
-  <a href = "https://www.google.com"> google </a>
-</div>
+  <Navbar/>
 
 </template>
 
+
+
+
 <style scoped>
-header {
-  line-height: 2;
-}
 
-main{
-  text-align: center;
-  margin-top:20px;
-  font-size: 36px;
-  font-family: 'Trebuchet MS', sans-serif;
-}
-
-.wrapper{
-      text-align: left;
-      margin-left: 50px;
-      font-size: 20px;
-  }
-.AT{
-  margin-left: 20px;
-}
-
-.navbar.navbar-dark.bg-dark{
-    background-color: #2f4ac4!important;
- }
 
  
-
-/*
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    font-size: 40px;
-    place-items: start start;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
 </style>
