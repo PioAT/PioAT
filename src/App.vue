5d98f5f198431aeebcf99c2b5630970e12647bc6
Justin Hu 321191 <321191@aaps.internal>
2023-02-09 11:04:08 -0500

    add description and styling

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
 <script setup>
 import Navbar from "~/src/components/Navbar.vue";
 
-
-
 </script>
 
 
@@ -13,11 +11,32 @@ import Navbar from "~/src/components/Navbar.vue";
 
   <main>
     Advanced Topics For Computer Science
-    <div class="wrapper"> Welcome to advanced topics in computer science, a </div>
+    <div class="description"> Welcome to advanced topics in computer science, an innovative course offered 
+       at Pioneer High School. Each year, students enroll in this class for the opportunity to 
+      explore deeper into their own interests in the computer science field.   </div>
+
+    <div class = "description2">
+      One of the best aspects of this course is the freedom to choose. From autonomous cars 
+      to quantum computing to game design, advanced topics has it all!
+    </div>
+
   </main>
 
   <div>
   <a href = "https://www.google.com"> google </a>
+  </div>
+
+  <div>
+  <b-button v-b-toggle.collapse-1 variant="primary">Toggle Collapse</b-button>
+  <b-collapse id="collapse-1" class="mt-2">
+    <b-card>
+      <p class="card-text">Collapse contents Here</p>
+      <b-button v-b-toggle.collapse-1-inner size="sm">Toggle Inner Collapse</b-button>
+      <b-collapse id="collapse-1-inner" class="mt-2">
+        <b-card>Hello!</b-card>
+      </b-collapse>
+    </b-card>
+  </b-collapse>
   </div>
 
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -37,9 +37,4 @@
   </header>
 
 
-
-
-
-
-
 </template>
