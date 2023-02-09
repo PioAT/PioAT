6eed29ba6e3b961c6d42b7edfa19fd3f3b8877be
Justin Hu 321191 <321191@aaps.internal>
2023-02-09 10:27:14 -0500

    move non-nav to app

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,16 @@ import Navbar from "~/src/components/Navbar.vue";
 <template>
   <Navbar/>
 
+
+  <main>
+    Advanced Topics For Computer Science
+    <div class="wrapper"> Welcome to advanced topics in computer science, a </div>
+  </main>
+
+  <div>
+  <a href = "https://www.google.com"> google </a>
+  </div>
+
 </template>
 
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,7 @@
 
 <template>
   <header>
-    <b-navbar toggleable="lg" type="dark" variant="dark" style = "background-color: aqua;" >
+    <b-navbar toggleable="lg" type="dark" variant="dark" style = "background-color: aqua;" class = "navbar" >
       <b-navbar-brand class = "AT" href="#">Advanced Topics</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -19,7 +19,7 @@
           <b-dropdown-item href="#">2024</b-dropdown-item>
           <b-dropdown-item href="#">2025</b-dropdown-item>
          </b-nav-item-dropdown>
-          <b-nav-item href="#">Course overview</b-nav-item>
+        
         </b-navbar-nav>
 
 
@@ -29,23 +29,17 @@
 
 
             <template #button-content>
-              <em>User</em>
+             
             </template>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
   </header>
 
-  <main>
-    Advanced Topics For Computer Science
-    <div class="wrapper"> Welcome to advanced topics in computer science, a </div>
-  </main>
 
 
 
-  <div>
 
-  <a href = "https://www.google.com"> google </a>
-</div>
+
 
 </template>
